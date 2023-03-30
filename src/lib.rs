58e9e47da297f452e99260650ec85b8a8f5fff2b
Grubre <ogrodowczykjakub@gmail.com>
2023-03-30 16:43:28 +0200

    refactor: Changed patterns to a Vec

--- src/lib.rs
+++ src/lib.rs
@@ -32,32 +32,32 @@ pub struct Token {
     pub line_nr: usize,
 }
 
-pub fn create_patterns() -> HashMap<TokenType, Regex> {
-    let mut patterns = HashMap::new();
+pub fn create_patterns() -> Vec<(TokenType, Regex)> {
+    let mut patterns = Vec::new();
 
-    patterns.insert(
+    patterns.push((
         TokenType::Mnemonic,
-        Regex::new(r"(?i)^(NOP|MOV|PUSH|POP|JMP|ADD|SUB|OR|AND|NEG|INV|SHR|SHL|CMP|HALT)").unwrap(),
+        Regex::new(r"(?i)^(NOP|MOV|PUSH|POP|JMP|ADD|SUB|OR|AND|NEG|INV|SHR|SHL|CMP|HALT)").unwrap()),
     );
-    patterns.insert(TokenType::Register, Regex::new(r"^(A|B)").unwrap());
-    patterns.insert(
+    patterns.push((TokenType::Register, Regex::new(r"^(A|B)").unwrap()));
+    patterns.push((
         TokenType::Number,
-        Regex::new(r"^(0x[0-9A-Fa-f]+|0b[01]+|0o[0-7]+|[0-9]+)$").unwrap(),
+        Regex::new(r"^(0x[0-9A-Fa-f]+|0b[01]+|0o[0-7]+|[0-9]+)$").unwrap()),
     );
-    patterns.insert(
+    patterns.push((
         TokenType::Label,
         Regex::new(r"^([a-zA-Z_][a-zA-Z0-9_]*):").unwrap(),
-    );
-    patterns.insert(
+    ));
+    patterns.push((
         TokenType::LabelRef,
         Regex::new(r"^#([a-zA-Z_][a-zA-Z0-9_]*)").unwrap(),
-    );
-    patterns.insert(TokenType::Comment, Regex::new(r"^;.*").unwrap());
+    ));
+    patterns.push((TokenType::Comment, Regex::new(r"^;.*").unwrap()));
 
     patterns
 }
 
-pub fn tokenize(patterns: &HashMap<TokenType, Regex>, input: &str) -> Vec<Token> {
+pub fn tokenize(patterns: &Vec<(TokenType, Regex)>, input: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
 
     for (i, line) in input.lines().enumerate() {
